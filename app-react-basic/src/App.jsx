d9980e44a5d2f487dc4d61726eaab0e55e0acbb9
Aeroxice <roymeyering@gmail.com>
2025-10-22 23:53:09 +0200

    Temperature App Finished. Works both ways.

--- app-react-basic/src/App.jsx
+++ app-react-basic/src/App.jsx
@@ -2,10 +2,18 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-const apps = ["Counter", "Temperature Converter", "No More Apps"]
+    //Switch Apps
+    const apps = ["Counter", "Temperature Converter", "No More Apps"]
     const [currentApp, setCurrentApp] = useState(0)
 
+    //Counter
+    const [count, setCount] = useState(0)
+
+    //Temperature Converter
+    const celsiusInput = document.getElementById("Celsius")
+    const fahrenheitInput = document.getElementById("Fahrenheit")
+
+    //Switch Apps
     function handleApp(){
         setCurrentApp(currentApp + 1)
         if(currentApp + 1 >= apps.length){
@@ -14,7 +22,21 @@ const apps = ["Counter", "Temperature Converter", "No More Apps"]
         }
     }
 
+    //Temperature Converter
+    function updateFahrenheit(){
+        const celsiusTemp = parseFloat(celsiusInput.value)
+        fahrenheitInput.value = parseFloat(celsiusTemp * (9/5) + 32)
+    }
+
+    function updateCelcius(){
+        const fahrenheitTemp = parseFloat(fahrenheitInput.value)
+        celsiusInput.value = parseFloat((fahrenheitTemp - 32) * (5/9))
+    }
+
+
+    //Switch Apps
     if (currentApp === 0){
+        //Counter
         return (
             <>
                 <h1>Roy Learns React</h1>
@@ -34,13 +56,14 @@ const apps = ["Counter", "Temperature Converter", "No More Apps"]
             </>
         )
     } else if (currentApp === 1){
+        //Temperature Converter
         return (
             <>
                 <h1>Roy Learns React</h1>
                 <h2>{apps[currentApp]}</h2>
                 <ul className={'list'}>
                     <li>
-                        <input type="number" id="Celsius" name="Celsius"/> Celsius = <input type="number" id="Fahrenheit" name="Fahrenheit"/> Fahrenheit
+                        <input type="number" id="Celsius" name="Celsius" onInput={updateFahrenheit}/> Celsius = <input type="number" id="Fahrenheit" name="Fahrenheit" onInput={updateCelcius}/> Fahrenheit
                     </li>
                     <li>
                         <button className={'button'} onClick={() => handleApp()}>
